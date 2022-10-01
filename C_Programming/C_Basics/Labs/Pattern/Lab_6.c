a27ce42c848ad03bc40a011d1f017aae396abe41
Reem Salah Shehab <114627885+reemsalah15@users.noreply.github.com>
2022-10-01 04:46:38 +0200

    Adding Lab_6.c

---
+++ C_Programming/C_Basics/Labs/Pattern/Lab_6.c
@@ -0,0 +1,22 @@
+/*
+ * Lab_6.c
+ *
+ *  Created on: Oct 1, 2022
+ *      Author: Reem Salah Shehab
+ */
+#include <stdio.h>
+void main()
+{
+
+	for(int i=9;i>=0;i--)
+	{
+		for(int j=9-i;j<=9;j++)
+		{
+		printf("%d ",j);
+		}
+		printf("\n");
+
+
+	}
+}
+
