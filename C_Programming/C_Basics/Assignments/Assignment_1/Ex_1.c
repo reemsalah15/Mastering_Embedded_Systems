219bd2e82b399da808a22f8049ee22a5a1c40472
Reem Salah Shehab <114627885+reemsalah15@users.noreply.github.com>
2022-10-01 17:01:24 +0200

    Adding Assignments file

---
+++ C_Programming/C_Basics/Assignments/Assignment_1/Ex_1.c
@@ -0,0 +1,12 @@
+/*
+ * Ex_1.c
+ *
+ *  Created on: Oct 1, 2022
+ *      Author: Reem Salah Shehab
+ */
+
+#include <stdio.h>
+void main()
+{
+	printf("C Programming");
+}
---
+++ C_Programming/C_Basics/Assignments/Assignment_1/README.md
@@ -0,0 +1,2 @@
+#  Print a Sentence "C Programming"
+## Example 1
